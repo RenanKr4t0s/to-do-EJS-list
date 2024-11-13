6208e6740716d4704e42d776734d09822fd8fc14
RenanKr4t0s <redrums88@gmail.com>
2024-11-13 19:04:52 -0300

    criando o dia.ejs e pondo em atividade

--- index.js
+++ index.js
@@ -16,19 +16,13 @@ app.set("view engine", 'ejs');
 //Pegando o dia de hoje
 const dia = new Date().getDay()
 
-
-app.get("/peole",(req,res)=>{
-    res.render("people", {foo:'Renan'})
-})
-
 app.get("/",(req,res)=>{
 
-    //trabalhando condição do dia de hoje
-    if(dia === 0 || dia === 6){
-        res.status(200).send("Estamos no fim de semana "+dia)        
-    }else{
-        res.status(200).sendFile(path.resolve('./views/index.html'))       
-    }    
+    const weekDay = (dia === 0 || dia === 6)? 
+    "Vamos descansar que é fds" :
+    "Bora trabalhar que é o momento"
+
+    res.status(200).render('dia',{day:weekDay})
 })
 
 app.listen(3000,()=>{
